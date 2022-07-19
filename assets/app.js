993a3f10cd61a754cd9f0446ed6307bd3bda336f
Alexandre Grué <alexandre.grue@hotmail.com>
2022-07-19 10:11:11 +0200

    edit

--- assets/app.js
+++ assets/app.js
@@ -14,8 +14,25 @@ import './styles/logginForm.scss';
 
 
 // start the Stimulus application
-/* import './bootstrap';
- */
+import './bootstrap';
+
+// app.js
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
+
+
 
 
 import AOS from 'aos';
